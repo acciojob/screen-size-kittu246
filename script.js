8f698bc93f63afbc3e08ad8b0ab61e0a09886142
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-25 12:58:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getWidth() {
+
+let section = document.getElementById("sizeInfo");
+let y = document.body.clientHeight;
+let x = document.body.clientWidth;
+section.innerHTML = `<h1> Width: ${x} and Height: ${y} </h1>`;
+
+}
